a715bdf1bf517ba4a198b4e7d98f069ddb4fcb12
AMenage <alexandermenage@hotmail.com>
2020-05-29 23:20:43 -0400

    Character locator and more hiragana characters.
    Javascript code modified to write location of stroke order and pronunciation audio file. Stroke order changes, but no audio file can be currently selected. Added more characters to hiragana cheatsheet.

--- kanaApp.js
+++ kanaApp.js
@@ -1,19 +1,46 @@
-// Pronunciation audio file storage
-var pro = 'hiragana/a/a.wav';
+// Cheatsheet reference variable; user starts in hiragana cheatsheet
+var hiraganaPage = true;
+
+// Character folder variables
+var charFolderAudio = 'hiragana/a/a.wav'; // Pronunciation audio file storage
+var charFolderStroke; 					  // Stroke order file storage
 
 // Play pronunciation of kana character
 function playAudio(aSource) {
-	var audio = new Audio(aSource); 
+	var audio = new Audio(aSource);
+	window.alert(aSource);
 	audio.play();
 }
 
 // Change selected kana character at side of browser
-function changeChar(strokeOrder, audioSource) {
-  document.getElementById("kanaChar").src = strokeOrder;
-  pro = audioSource;
+function changeChar(theChar) {
+  if (hiraganaPage == true) {
+	  charFolderAudio = "hiragana/" + theChar + "/" + theChar + ".wav";
+	  charFolderStroke = "hiragana/" + theChar + "/" + theChar + "-Hira-stroke.png";
+  }
+  else {
+	  charFolderAudio = "katakana/" + theChar + "/" + theChar + ".wav";
+	  charFolderStroke = "katakana/" + theChar + "/" + theChar + "-Kata-stroke.png";
+  }
+  document.getElementById("kanaChar").src = charFolderStroke;
+  playAudio(charFolderAudio);
 }
 
 // Play stored pronunciation
 function pronounce() {
-	playAudio(pro);
+	var audio = new Audio(charFolderAudio);
+	audio.play();
+}
+
+// Check if user is in hiragana or katakana page
+function pageCheck(pCon) {
+	if (pCon == "hira") {
+		var hiraganaPage = true;
+	}
+	else if (pCon == "kata") {
+		var hiraganaPage = false;
+	}
+	else {
+		window.alert("Error: Page not available."); // For unspecified page
+	}
 }
