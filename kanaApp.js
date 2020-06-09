51bb6cf7d68db5fd35e1a42c034769324d466e7f
MenAlex <alexandermenage@hotmail.com>
2020-06-09 12:49:21 -0400

    Javascript checks current page.
    The script can now check whether the page is the hiragana cheatsheet or the katakana cheatsheet. New characters added to katakana cheatsheet for demonstration purposes.

--- kanaApp.js
+++ kanaApp.js
@@ -1,10 +1,4 @@
-// Array of kana cheatsheet locations
-var pageArray = [hiragana.html, katakana.html]; 
-var currentPage; 					  // Cheatsheet filler
-// Check for current cheatsheet
-/*if (window.location.href == pageArray[0]) {
-	currentPage = "hira";
-} else if*/ 
+var hiraganaPage = document.URL.includes("hiragana.html"); // Current page condition
 
 // Character folder variables
 var charFolderAudio = 'hiragana/a/a.wav'; // Pronunciation audio file storage
@@ -28,9 +22,4 @@ function changeChar(theChar) {
 function pronounce() {
 	var audio = new Audio(charFolderAudio);
 	audio.play();
-}
-
-// Button for debugging
-function bugger() {
-	alert(document.URL);
 }
